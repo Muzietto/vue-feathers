54a637d419dfd41f85b38a52de98fc3f71a1d991
Marco Faustinelli (Muzietto) <marco_faustinelli@yahoo.com>
2018-05-30 18:41:22 +0200

    products into vue vieww

---
+++ vue/json-data-feathers/client.js
@@ -0,0 +1,12 @@
+(function(w) {
+  const app = feathers();
+
+  const restClient = feathers.rest('http://localhost:3030')
+
+  // Configure an AJAX library (see below) with that client 
+  app.configure(restClient.fetch(window.fetch));
+
+  // Connect to the `http://feathers-api.com/messages` service
+  w.restClient = app.service('json');
+  
+}(window));
