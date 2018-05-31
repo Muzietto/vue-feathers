ad660f25c7f631dd874ccffd351f57566d511054
Marco Faustinelli (Muzietto) <marco_faustinelli@yahoo.com>
2018-05-31 17:18:54 +0200

    button pushes home

--- vue/vue-router-tutorial/src/components/PageTwo.vue
+++ vue/vue-router-tutorial/src/components/PageTwo.vue
@@ -4,9 +4,17 @@
         <small class="text-muted">I am page 2 TWO!</small>
     </h1>
     <a class="btn btn-primary btn-lg" role="button"><router-link to="/">Bacco Tabacco</router-link></a>
+    <p>An example of using this.$router.push()</p>
+    <button @click="pushHome" class="btn">Go Home</button>
   </div>
 </template>
  
 <script>
-    export default {}
+  export default {
+      methods: {
+          pushHome() {
+              this.$router.push('/');
+          }
+      }
+  }
 </script>
