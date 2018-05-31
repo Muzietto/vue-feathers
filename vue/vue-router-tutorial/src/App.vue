9527fbb85dacd76ab6e8580484d05c501ad46d3a
Marco Faustinelli (Muzietto) <marco_faustinelli@yahoo.com>
2018-05-31 17:36:52 +0200

    some flickering animation, blah....

--- vue/vue-router-tutorial/src/App.vue
+++ vue/vue-router-tutorial/src/App.vue
@@ -2,7 +2,9 @@
     <div class="container">
         <h1>Vue Routing Example</h1>
         <navigation></navigation>
-        <router-view></router-view>
+        <transition name="slide">
+          <router-view></router-view>
+        </transition>
     </div>
 </template>
  
@@ -16,4 +18,37 @@
     }
 </script>
  
+<style>
+    .slide-leave-active {
+        transition: opacity 1s ease;
+        opacity: 0;
+        animation: slide-out 1s ease-out forwards;
+    }
+ 
+    .slide-leave {
+        opacity: 1;
+        transform: translateX(0);
+    }
+ 
+    .slide-enter-active {
+        animation: slide-in 1s ease-out forwards;
+    }
 
+   @keyframes slide-out {
+        0% {
+            transform: translateY(0);
+        }
+        100% {
+            transform: translateY(-30px);
+        }
+    }
+ 
+    @keyframes slide-in {
+        0% {
+            transform: translateY(-30px);
+        }
+        100% {
+            transform: translateY(0);
+        }
+    }    
+</style>
